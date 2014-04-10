e04970c8a49f7213bf45a7fb21c8e85a1d423e59
QIYUAN LUO <luoq@lonsdale01.cluster>
2014-04-10 18:25:45 +0100

    euler heston

---
+++ 5HESTON/euler-heston.cc
@@ -0,0 +1,108 @@
+#include<iostream>
+#include<fstream>
+#include<gsl/gsl_rng.h>
+#include<gsl/gsl_randist.h>
+#include<cmath>
+#define N 512 
+#define MC 1000 // times of monte carlo tests..
+
+using namespace std;
+
+bool cor_check(double dw1[N], double dw2[N]) // check if the two processes are correlated..
+{
+    double sum12=0, sqsum1=0, sqsum2=0, sum1=0, sum2=0, w1[N], w2[N];
+    sum1 = w1[0] = dw1[0], sum2 = w2[0] = dw2[0];
+    sqsum1 = w1[0]*w1[0], sqsum2 = w2[0]*w2[0];
+    sum12 = w1[0]*w2[0];
+
+    for(int i=1; i<N; ++i)
+    {
+        w1[i] = w1[i-1] + dw1[i-1];
+        sum1 = sum1 + w1[i];
+        sqsum1 = sqsum1 + w1[i]*w1[i];
+
+        w2[i] = w2[i-1] + dw2[i-1];
+        sum2 = sum2 + w2[i];
+        sqsum2 = sqsum2 + w2[i]*w2[i];
+        
+        sum12 = sum12 + w1[i]*w2[i];
+    }
+    double rho = (sum12/N - sum1*sum2/N/N) / sqrt( sqsum1/N - sum1*sum1/N/N ) / sqrt( sqsum2/N - sum2*sum2/N/N );
+    
+    if (rho > 0.4 ) 
+    {
+        return 0; // if rho is larger than 0.4, this path is usable, return 0 to exit the loop..
+    }
+    else return 1; // else return 1, generate dw1 and dw2 again and check the if correlated..
+}
+
+void generate_W(gsl_rng *r, double w1[N], double w2[N], double dt)
+{
+    for(int i=0; i<N; ++i)
+    {
+       w1[i] = sqrt(dt) * gsl_ran_gaussian(r, 1.0); 
+       w2[i] = sqrt(dt) * gsl_ran_gaussian(r, 1.0); 
+    }
+}
+
+int main()
+{
+gsl_rng *r = gsl_rng_alloc( gsl_rng_mt19937);
+gsl_rng_env_setup();
+gsl_rng_set(r, time(NULL));
+
+const double T=1.0, rate = 0.05, kappa = 1.2, theta = 0.04, eps = 0.3, dt = T/N;
+double dW1[N], dW2[N], X[N], V[N]; 
+
+const double strike_price = 120;
+double strike_sum = 0;
+
+ofstream file("plot.dat");
+for(int j=0; j<MC; ++j)
+{
+    bool check = 1;
+    while(check)
+    {
+        generate_W(r, dW1, dW2, dt);
+        check = cor_check(dW1, dW2);
+    }
+    
+    V[0] = 0.04, X[0] = 100;
+    for(int i=1; i<N; ++i)
+    {
+        //X[i] = X[i-1] + rate * X[i-1] * dt + sqrt(V[i-1]) * X[i-1] * dW1[i-1];
+        X[i] = X[i-1] + rate * X[i-1] * dt + sqrt(abs(V[i-1])) * X[i-1] * dW1[i-1];
+        
+        //V[i] = V[i-1] + kappa * dt * (theta - V[i-1]) + eps * sqrt(V[i-1]) * dW2[i-1];
+        V[i] = V[i-1] + kappa * dt * (theta - V[i-1]) + eps * sqrt(abs(V[i-1])) * dW2[i-1];
+    }
+    file<<j<<" "<<X[N-1]<<" "<<V[N-1]<<endl;
+
+    if( X[N-1] > strike_price )
+    {
+        strike_sum = strike_sum + X[N-1];
+    }
+}
+file.close();
+gsl_rng_free(r);
+
+cout<<"This is a Call Option at "<<strike_price<<"$"<<endl<<"Option price is "<<exp(-rate * T) * strike_sum/MC<<endl;
+
+/*=============== plotting part ===============*/
+
+
+FILE *gp = popen("gnuplot -persist", "w");;
+
+if(gp == NULL) 
+{
+    printf("Cannot plot the data!\n");
+    exit(0);
+}
+
+fprintf(gp, "set yrange[-10:150]\n");
+fprintf(gp, "plot 'plot.dat' u 1:2 w l, 'plot.dat' u 1:3 w l\n");
+fprintf(gp, "pause -1\n");
+fclose(gp);
+
+return 0;
+}
